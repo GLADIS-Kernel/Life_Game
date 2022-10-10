f07db52ce1d7c18310922ddc10afa84196af0c2f
GLADIS-Kernel <arnausastremartinez@gmail.com>
2022-10-10 21:26:27 +0200

    Commit numero 1 nomes per mirar que tot funciona corréate

---
+++ test1.c
@@ -0,0 +1,12 @@
+//
+//  test1.c
+//  
+//
+//  Created by Arnau Sastre on 10/10/22.
+//
+
+#include "test1.h"
+
+void hello(){
+    sprintf("hello world")
+}
---
+++ test1.h
@@ -0,0 +1,15 @@
+//
+//  test1.h
+//  
+//
+//  Created by Arnau Sastre on 10/10/22.
+//
+
+#ifndef test1_h
+#define test1_h
+
+#include <stdio.h>
+
+
+
+#endif /* test1_h */
