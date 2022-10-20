e41143f3866c18549fa05f852a26888e6d4ae408
GLADIS-Kernel <arnausastremartinez@gmail.com>
2022-10-20 19:13:50 +0200

    commit 3

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <ncurses.h>
 #include "test1.h"
 
@@ -14,30 +15,53 @@ int main(int argc, const char * argv[]) {
 
     initscr();            /* Start curses mode           */
     
-    struct cell {
-        int alive;
-        int pos_x;
-        int pos_y;
-        
-    };
+
+    typedef struct {
+    int alive ;
+    int pos_x;
+    int pos_y; } cell;
     
-    int width = 50;
-    int height = 50;
+    int width = 4;
+    int height = 4;
+    
+    int fsize = 4;
+    
+    cell ** field = (cell **) malloc(fsize * sizeof(cell *));
+    
+    for (int i = 0; i < fsize; ++i)
+        field[i] = (cell *)malloc(fsize * sizeof(cell));
     
     
-    struct cell c1; // declares two cell variables // of type stru
-    c1.pos_x = 30; // sets x coordinate of cell 1 to 30
-    c1.pos_y = 50; // sets x coordinate of cell 1 to 50
-    c1.alive = 1; // cell 1 is alive
+    /*field [0][0].alive = 1;
+    field [0][0].pos_x = 1;
+    field [0][0].pos_y = 1;
+
+    printw("My cell at 0,0 is at state: %d\n", field[0][0].alive);
+    printw("My coordinates for cell at 0,0 are: %d,%d\n",field [0][0]. pos_x , field [0][0]. pos_y );*/
     
+
+    /*printw("Hello World !!!\n");     Print Hello World
+    int d = hello();
+    printw("x equals %i",d);*/
     
-    printw("Hello World !!!");    /* Print Hello World          */
-    int d =hello();
-    printw("%i",d);
-    printw("%i",c1.pos_x);
+    //INIT CELL Matrix
+    int x = 0;
+    while (x<50){
+        for (int i = 0; i<fsize; ++i){
+            for (int j = 0; j<fsize; ++i){
+                field[i][j].pos_x=i;
+                field[i][j].pos_y=j;
+                field[i][j].alive=0;
+            }
+        }
+    }
     refresh();            /* Print it on to the real screen */
     getch();            /* Wait for user input */
+    
+    
+    
     endwin();           /* End curses mode          */
     
     return 0;
 }
+
